e80c54268eca7eb60d558493dea7f32745940ca8
Leavedream <mengliu@yahoo.com>
2017-06-08 14:37:07 -0400

    import

---
+++ src/main/java/com/vendingmachine/Test.java
@@ -0,0 +1,10 @@
+package com.vendingmachine;
+
+/**
+ * Dummy class to test that project compiles
+ */
+public class Test {
+	public static void main( String args[] ) {
+		System.out.println( "Test" );
+	}
+}
